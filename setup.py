13b47616d4b7393a71f026b87a99db09376ab508
mhchia <kevin.mh.chia@gmail.com>
2017-12-20 17:05:43 +0800

    Modify `eth_tester` version in setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
         "rlp==0.4.7",
         "eth-keys==0.1.0b3",
         "trie>=0.3.1",
-        "eth-tester>=0.1.0b5",
+        "eth-tester>=0.1.0b6",
         "web3>=4.0.0b2",
     ],
     extra_require={
