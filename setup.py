f12f036acaaa50733c8946d77ea1919f7759ba5f
mhchia <kevin.mh.chia@gmail.com>
2018-01-04 16:36:07 +0800

    Test py-trie version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
         "py-ecc==1.4.2",
         "rlp==0.4.7",
         "eth-keys==0.1.0b3",
-        "trie>=0.3.1",
+        "trie==0.3.2",
         "eth-tester>=0.1.0b6",
         "web3>=4.0.0b5",
     ],
